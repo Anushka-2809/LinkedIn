e5c61b7e5e666765dad9acf8ef475e6d84ba6e00
Anushka Maity <maityanushka60@gmail.com>
2026-02-12 13:19:36 +0530

    baseNode

--- frontend/src/nodes/inputNode.js
+++ frontend/src/nodes/inputNode.js
@@ -1,47 +1,28 @@
-// inputNode.js
-
-import { useState } from 'react';
-import { Handle, Position } from 'reactflow';
+import { useState } from "react";
+import { BaseNode } from "./baseNode";
 
 export const InputNode = ({ id, data }) => {
-  const [currName, setCurrName] = useState(data?.inputName || id.replace('customInput-', 'input_'));
-  const [inputType, setInputType] = useState(data.inputType || 'Text');
+  const [inputName, setInputName] = useState(data?.inputName || "input");
 
-  const handleNameChange = (e) => {
-    setCurrName(e.target.value);
-  };
-
-  const handleTypeChange = (e) => {
-    setInputType(e.target.value);
+  const handleChange = (e) => {
+    setInputName(e.target.value);
   };
 
   return (
-    <div style={{width: 200, height: 80, border: '1px solid black'}}>
-      <div>
-        <span>Input</span>
-      </div>
-      <div>
-        <label>
-          Name:
-          <input 
-            type="text" 
-            value={currName} 
-            onChange={handleNameChange} 
-          />
-        </label>
-        <label>
-          Type:
-          <select value={inputType} onChange={handleTypeChange}>
-            <option value="Text">Text</option>
-            <option value="File">File</option>
-          </select>
-        </label>
-      </div>
-      <Handle
-        type="source"
-        position={Position.Right}
-        id={`${id}-value`}
-      />
-    </div>
+    <baseNode
+      id={id}
+      title="Input"
+      inputs={[]}              // no left handle
+      outputs={["output"]}     // one right handle
+    >
+      <label>
+        Name:
+        <input
+          type="text"
+          value={inputName}
+          onChange={handleChange}
+        />
+      </label>
+    </baseNode>
   );
-}
+};
