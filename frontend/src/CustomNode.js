2a80bf8676563855bd690554b28a6ceb1abb491e
Anushka Maity <maityanushka60@gmail.com>
2026-02-13 10:46:06 +0530

    ..

---
+++ frontend/src/CustomNode.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const CustomNode = ({ data }) => {
+  const [minimized, setMinimized] = useState(false);
+
+  return (
+    <div className="base-node" style={{ width: 200 }}>
+      <div className="node-title">
+        {data.label}
+        <div style={{ float: "right" }}>
+          <button onClick={() => setMinimized(!minimized)}>
+            {minimized ? "Max" : "Min"}
+          </button>
+          <button>⚙</button>
+          <button>❌</button>
+        </div>
+      </div>
+      {!minimized && <input placeholder="Value" />}
+    </div>
+  );
+};
+
+export default CustomNode;
